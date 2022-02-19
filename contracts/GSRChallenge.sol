2229f894f3d4bdcfa86f6c1a15f88b28a5c1fe56
Francis_ldn <fnhh04@gmail.com>
2022-02-19 16:25:10 +0000

    add input validation

--- contracts/GSRChallenge.sol
+++ contracts/GSRChallenge.sol
@@ -50,6 +50,12 @@ interface IUniswapV2Router02 {
 }
 
 contract GSRChallenge2PoolArbitrage {
+    /// @notice enable user to get max arbitrage between 2 pools using getPrice2 function
+    /// @param _token0 address of token0
+    /// @param _token1 address of token1
+    /// @param _router0 address of Dex0 router
+    /// @param _router1 address of Dex1 router
+    /// @param _amount0In input amount of token0, amount can have implications on the slippage
     function maxArbitragePossible(
         address _token0,
         address _token1,
@@ -66,11 +72,27 @@ contract GSRChallenge2PoolArbitrage {
         )
     {
         // Implement code to calculate the maximum amount of arbitrage that can be traded for
+        require(
+            _token0 != address(0) && _token1 != address(0),
+            "invalid_token_address"
+        );
+        require(_amount0In > 0, "invalid_input_amount");
+        require(
+            _router0 != address(0) && _router1 != address(0),
+            "invalid_router_address"
+        );
         price0 = _getPrice2(_token0, _token1, _router0, _amount0In);
         price1 = _getPrice2(_token0, _token1, _router1, _amount0In);
+        require(price0 > 0 && price1 > 0, "invalid_prices");
         arb = price0 >= price1 ? (price0 - price1) : (price1 - price0);
     }
 
+    ///@notice enable user to execute arbitrage of a token pair between 2 Dexes
+    /// @param _token0 address of token0
+    /// @param _token1 address of token1
+    /// @param _router0 address of Dex0 router
+    /// @param _router1 address of Dex1 router
+    /// @param _amount0 input amount of token0, amount can have implications on the slippage
     function executeArb(
         address _token0,
         address _token1,
@@ -78,6 +100,15 @@ contract GSRChallenge2PoolArbitrage {
         address _router1,
         uint256 _amount0
     ) external returns (uint256 amountOut, uint256 arbProfit) {
+        require(
+            _token0 != address(0) && _token1 != address(0),
+            "invalid_token_address"
+        );
+        require(_amount0 > 0, "invalid_input_amount");
+        require(
+            _router0 != address(0) && _router1 != address(0),
+            "invalid_router_address"
+        );
         (uint256 price0, uint256 price1, ) = maxArbitragePossible(
             _token0,
             _token1,
@@ -115,25 +146,33 @@ contract GSRChallenge2PoolArbitrage {
         }
     }
 
+    ///@notice enable user to withdraw all token balance in the contract
     function withdrawToken(address token, address to) public {
+        require(token != address(0), "invalid_token_address");
+        require(to != address(0), "invalid_recipient_address");
         require(
             ERC20(token).transfer(to, ERC20(token).balanceOf(address(this))),
             "transfer_failed"
         );
     }
 
+    ///@notice view only function that enables user to get token balance in this contract
     function getTokenBalance(address token) external view returns (uint256) {
+        require(token != address(0), "invalid_token_address");
         return ERC20(token).balanceOf(address(this));
     }
 
+    ///@notice enable user to deposit tokens to the contract
     function _depositToken(address token, uint256 amount) public {
+        require(token != address(0), "invalid_token_address");
+        require(amount > 0, "invalid_deposit_amount");
         require(
             ERC20(token).transferFrom(msg.sender, address(this), amount),
             "transfer_failed"
         );
     }
 
-    // swap between 2 ERC20 tokens through router.swapExactTokensForTokens() function
+    ///@notice user can swap between 2 ERC20 tokens through router.swapExactTokensForTokens() function
     function _swapToken(
         address token0,
         address token1,
@@ -144,7 +183,7 @@ contract GSRChallenge2PoolArbitrage {
         require(routerAdd != address(0), "invalid_address");
         require(
             token0 != token1 && token0 != address(0) && token1 != address(0),
-            "invalid pair"
+            "invalid_token_address"
         );
         address[] memory path = new address[](2);
         path[0] = token0;
@@ -162,24 +201,41 @@ contract GSRChallenge2PoolArbitrage {
         require(amount1 > 0, "swap_failed");
     }
 
-    // get liquidity pool Pair address from factory contract
+    ///@notice get liquidity pool address from factory contract
     function _getPairAddress(
         address _token0,
         address _token1,
         address _factory
-    ) public view returns (address) {
+    ) public view returns (address pool) {
+        require(
+            _token0 != _token1 &&
+                _token0 != address(0) &&
+                _token1 != address(0),
+            "invalid_token_address"
+        );
+        require(_factory != address(0), "invalid_factory_address");
         IUniswapV2Factory factory = IUniswapV2Factory(_factory);
-        return factory.getPair(_token0, _token1);
+        pool = factory.getPair(_token0, _token1);
+        require(pool != address(0), "invalid_pool_address");
+        return pool;
     }
 
-    // get exchange rate for token0/token1 from Pair reserves
+    ///@notice get exchange rate for token0/token1 from Pair reserves, but this is not used
     function _getPrice(
         address _token0,
         address _token1,
         address factory
     ) public view returns (uint256) {
+        require(
+            _token0 != address(0) &&
+                _token1 != address(0) &&
+                _token0 != _token1,
+            "invalid_token_address"
+        );
+        require(factory != address(0), "invalid_factory_address");
+
         address pairAddress = _getPairAddress(_token0, _token1, factory);
-        require(pairAddress != address(0), "Pool_does_not_exist");
+        require(pairAddress != address(0), "invalid_pool_address");
         IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
 
         (uint256 res0, uint256 res1, ) = pair.getReserves();
@@ -188,13 +244,22 @@ contract GSRChallenge2PoolArbitrage {
         return (res0 * PRICE_DECIMALS) / res1;
     }
 
-    // get exchange rate for token0/token1 from router.getAmountsOut() function
+    /// @notice get exchange rate for token0/token1 from router.getAmountsOut() function
     function _getPrice2(
         address _token0,
         address _token1,
         address routerAdd,
         uint256 amountIn
     ) public view returns (uint256) {
+        require(
+            _token0 != address(0) &&
+                _token1 != address(0) &&
+                _token0 != _token1,
+            "invalid_token_address"
+        );
+        require(routerAdd != address(0), "invalid_factory_address");
+        require(amountIn > 0, "invalid_input_amount");
+
         IUniswapV2Router02 router = IUniswapV2Router02(routerAdd);
         address[] memory path = new address[](2);
         path[0] = _token0;
